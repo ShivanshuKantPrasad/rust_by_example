435431fa785a18252737a8f11f39ee214102d12f
Shivanshu Kant Prasad <shivanshukantprasad@gmail.com>
2022-05-27 18:29:23 +0530

    Testcase: Linked Lists

---
+++ linked_list.rs
@@ -0,0 +1,50 @@
+use crate::List::*;
+
+#[derive(Debug)]
+enum List {
+    // Cons: Tuple struct that wraps and element and a pointer to the next node
+    Cons(u32, Box<List>),
+    // Nil: A node that signifies the end of the linked list
+    Nil,
+}
+
+impl List {
+    fn new() -> List {
+        Nil
+    }
+
+    // Consume a list, and return the same list with a new element at its front
+    fn prepend(self, elem: u32) -> List {
+        Cons(elem, Box::new(self))
+    }
+
+    fn len(&self) -> u32 {
+        match *self {
+            Cons(_, ref tail) => 1 + tail.len(),
+            Nil => 0
+        }
+    }
+
+    fn stringify(&self) -> String {
+        match *self {
+            Cons(head, ref tail) => {
+                format!("{}, {}", head, tail.stringify())
+            },
+            Nil => {
+                format!("Nil")
+            }
+        }
+    }
+}
+
+fn main() {
+    let mut list = List::new();
+
+    list = list.prepend(1);
+    list = list.prepend(2);
+    list = list.prepend(3);
+
+    println!("linked list has length: {}", list.len());
+    println!("{}", list.stringify());
+
+}
