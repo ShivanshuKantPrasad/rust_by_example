5b14fd0958db63d16ce8d7ecf9921c102c1ef52e
Shivanshu Kant Prasad <shivanshukantprasad@gmail.com>
2022-06-04 16:33:04 +0530

    Declare first

--- src/bin/debug.rs
+++ src/bin/debug.rs
@@ -7,17 +7,19 @@ struct Structure(String);
 #[derive(Debug)]
 struct Deep(Structure);
 
-fn main(){
+fn main() {
     println!("{:?} months in a year.", 12);
-    println!("{1:?} {0:?} is the {actor:?} name.",
-             "Slater",
-             "Christian",
-             actor="actor's");
+    println!(
+        "{1:?} {0:?} is the {actor:?} name.",
+        "Slater",
+        "Christian",
+        actor = "actor's"
+    );
 
     // `Structure` is printable!
     println!("Now {:?} will print!", Structure("3".to_string()));
     println!("Now {:#?} will print!", Structure("3".to_string()));
-    
+
     // The problem with `derive` is there is no control over how
     // the results look. What if I want this to just show a `7`?
     println!("Now {:?} will print!", Deep(Structure("7".to_string())));
